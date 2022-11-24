1132cbc626687f913204a69e977e8e094aa76492
ArtChaos189 <tvolkiska967@gmail.com>
2022-11-25 00:48:46 +0300

    m firs commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Sekelton";
+import Pagination from "../components/Pagination";
+
+const Home = ({ searchValue }) => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sort, setSort] = React.useState({
+    name: "↑популярности",
+    sortProperty: "rating",
+  });
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  React.useEffect(() => {
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const sortBy = sort.sortProperty.replace("-", "");
+
+    setIsLoading(true);
+    fetch(
+      `https://637c882872f3ce38eaa4fa33.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`
+    )
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sort, searchValue, currentPage]);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories onClickCategory={(i) => setCategoryId(i)} value={categoryId} />
+        <Sort onClickSort={(i) => setSort(i)} sortValue={sort} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items
+              .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+              .map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+    </div>
+  );
+};
+
+export default Home;
